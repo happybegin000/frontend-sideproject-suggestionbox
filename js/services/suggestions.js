da5a513defb69d0c5234fd560898b277a6b5663f
Reyes H <reyesh@gmail.com>
2016-07-11 08:29:07 -0700

    FEAT: angular setup

---
+++ js/services/suggestions.js
@@ -0,0 +1,25 @@
+app.factory('places', ['$http', function($http) {
+	var demoSuggestions = {
+		posts: [
+			{
+				title: 'Free Pizza at club meetings',
+				upvotes: 15,
+				comments: []
+			},
+			{
+				title: 'Nirvana the rock band',
+				upvotes: 15,
+				comments: []
+			},			{
+				title: 'Coporate rock sucks',
+				upvotes: 15,
+				comments: []
+			},
+			{
+				title: 'I have 12 consoles',
+				upvotes: 15,
+				comments: []
+			}		
+		]
+	}
+}]);
